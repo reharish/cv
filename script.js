35ff8a4d38259e0fb57544f26f6f5141658677f4
harishbabu.r <harishbabu.r@zilogic.com>
2022-01-20 17:03:39 +0530

    Added : TimeOut script

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
 function delayRedirect(){
-    document.getElementById('delayMsg').innerHTML = 'Redirect in <span id="countDown">10</span> seconds..';
+    document.getElementById('delayMsg').innerHTML = '<h4>Redirect in <span id="countDown">10</span> seconds..</h4>';
     var count = 10;
     setInterval(function(){
         count--;
         document.getElementById('countDown').innerHTML = count;
 	console.log(count)
         if (count == 0) {
-            window.location = 'https://www.google.com'; 
+            window.location = 'https://reharish.github.io/'; 
         }
     },1000);
 }
-/*
-</script>
-<body onload="delayRedirect()">
-<div id="delayMsg">This is it</div>
-<input type="button" onclick="delayRedirect()" value="Click to Redirect"/>
-</body>
-
-</html>
-*/
