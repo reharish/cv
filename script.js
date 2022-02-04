6d33e37bd9df97944f61fcea9e3b1f20142e7935
reharish <58879939+reharish@users.noreply.github.com>
2022-02-04 22:37:36 +0530

    update: Msg changed

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /* Used for delay Redirect and also show timer on screen */
 
 function delayRedirect(){
-    document.getElementById('delayMsg').innerHTML = '<h4>Redirect in <span id="countDown">10</span> seconds..</h4>';
-    var count = 10;
+    document.getElementById('delayMsg').innerHTML = '<h4>Portfolio Loading .... <span id="countDown">10</span> seconds..</h4>';
+    var count = 8;
     setInterval(function(){
         count--;
         document.getElementById('countDown').innerHTML = count;
